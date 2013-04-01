24cb331f5317af7f7c31ecdc9a860b39ae35452d
Ian Philpot <ian.philpot@microsoft.com>
2013-04-01 14:36:14 -0400

    Updated and Created funcitons
    Updated and created funcitons Get-SMPLDomains, Get-SMPLDomainRecords,
    Add-SMPLDomainRecord, Update-SMPLDomainRecord, Remove-SMPLDomainRecord

--- PowerDNSimple.psm1
+++ PowerDNSimple.psm1
@@ -30,6 +30,36 @@ function GetRequest
     return $json
 }
 
+function PostRequest
+{
+    param(
+        $url,
+        $jsonData,
+        $emailAddress,
+        $domainApiToken
+    )
+
+    [System.Net.HttpWebRequest]$request = [System.Net.WebRequest]::Create($url)
+    $request.Accept = "application/json"
+    $request.ContentType = "application/json" 
+    $request.Method = "POST"
+    $dnsimpleToken = $emailAddress + ":" + $domainApiToken
+    $request.Headers.add("X-DNSimple-Token", $dnsimpleToken)
+    $request.ContentLength = $jsonData.ToString().length
+
+    [System.IO.StreamWriter]$writer = $request.GetRequestStream()
+    $writer.Write($jsonData)
+    $writer.Close()
+
+    $response = $request.GetResponse()
+    $stream = $response.GetResponseStream()
+    $reader = New-object System.IO.StreamReader($stream)
+    $json = ConvertFrom-Json ($reader.ReadToEnd())
+    $reader.Dispose()
+
+    return $json
+}
+
 function PutRequest
 {
     param(
@@ -59,15 +89,68 @@ function PutRequest
 
     return $json
 }
+
+function DeleteRequest
+{
+    param(
+        $url,
+        $emailAddress,
+        $domainApiToken
+    )
+
+    [System.Net.HttpWebRequest]$request = [System.Net.WebRequest]::Create($url)
+    $request.Accept = "application/json"
+    $request.ContentType = "application/json" 
+    $request.Method = "DELETE"
+    $dnsimpleToken = $emailAddress + ":" + $domainApiToken
+    $request.Headers.add("X-DNSimple-Token", $dnsimpleToken)
+
+    $response = $request.GetResponse()
+    $stream = $response.GetResponseStream()
+    $reader = New-object System.IO.StreamReader($stream)
+    $json = ConvertFrom-Json ($reader.ReadToEnd())
+    $reader.Dispose()
+
+    return $json
+}
+#endregion
+
+#region Domains
+function Get-SMPLDomains
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        $emailAddress,
+        [Parameter(Mandatory = $true)]
+        $domainApiToken,
+        [Switch]
+        $PassThru
+    )
+
+    $url = "https://dnsimple.com/domains"
+
+    $response = GetRequest -url $url -emailAddress $emailAddress -domainApiToken $domainApiToken
+
+    if ($passThru)
+    {
+        return $response
+    }
+    else
+    {
+        $response.domain
+    }
+}
 #endregion
 
 #region Domain Records
 function Get-SMPLDomainRecords
 {
     param(
-        [Parameter(ValueFromPipeline = $true)]
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
         $domain,
+        [Parameter(Mandatory = $true)]
         $emailAddress,
+        [Parameter(Mandatory = $true)]
         $domainApiToken,
         [Switch]
         $PassThru
@@ -87,18 +170,85 @@ function Get-SMPLDomainRecords
     }
 }
 
-function Update-SMPLDomainRecord
+function Add-SMPLDomainRecord
 {
     param(
+        [Parameter(Mandatory = $true)]
         $name,
+        [Parameter(Mandatory = $true)]
+        $recordType,
+        [Parameter(Mandatory = $true)]
         $content,
         $ttl,
         $prio,
-        [Parameter(ValueFromPipeline = $true)]
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
         $domain,
-        [Parameter(ValueFromPipeline = $true)]
-        $recordID,
+        [Parameter(Mandatory = $true)]
         $emailAddress,
+        [Parameter(Mandatory = $true)]
+        $domainApiToken,
+        [Switch]
+        $passThru
+    )
+
+    $url = "https://dnsimple.com/domains/$domain/records"
+    $items = @{"record"=@{}}
+
+    if ($name)
+    {
+        $items.record.name = $name
+    }
+
+    if ($recordType)
+    {
+        $items.record.record_type = $recordType
+    }
+
+    if ($content)
+    {
+        $items.record.content = $content
+    }
+
+    if ($ttl)
+    {
+        $items.record.ttl = $ttl
+    }
+
+    if ($prio)
+    {
+        $items.record.prio = $prio
+    }
+
+    $jsonData = $items | ConvertTo-Json
+
+    $response = PostRequest -url $url -jsonData $jsonData -emailAddress $emailAddress -domainApiToken $domainApiToken
+
+    if ($passThru)
+    {
+        return $response
+    }
+    else
+    {
+        $response.record
+    }
+}
+
+function Update-SMPLDomainRecord
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        $name,
+        [Parameter(Mandatory = $true)]
+        $content,
+        $ttl,
+        $prio,
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
+        $domain,
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
+        $recordID,
+        [Parameter(Mandatory = $true)]
+        $emailAddress,
+        [Parameter(Mandatory = $true)]
         $domainApiToken,
         [Switch]
         $passThru
@@ -140,6 +290,42 @@ function Update-SMPLDomainRecord
         $response.record
     }
 }
+
+function Remove-SMPLDomainRecord
+{
+    param(
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
+        $domain,
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
+        $recordID,
+        [Parameter(Mandatory = $true)]
+        $emailAddress,
+        [Parameter(Mandatory = $true)]
+        $domainApiToken,
+        [Switch]
+        $passThru
+    )
+
+    $a = Read-Host "Are you sure you want to delete this record? (Y/N)"
+    $url = "https://dnsimple.com/domains/$domain/records/$recordID"
+
+    if ($a -eq "Y")
+    {
+        $response = DeleteRequest -url $url -emailAddress $emailAddress -domainApiToken $domainApiToken
+        if ($passThru)
+        {
+            return $response
+        }
+        else
+        {
+            $response.record
+        }
+    }
+    else
+    {
+        Write-Host "Operation aborted" -ForegroundColor Yellow
+    }
+}
 #endregion
 
-Export-ModuleMember -Function Update-SMPLDomainRecord, Get-SMPLDomainRecords
+Export-ModuleMember -Function Get-SMPLDomains, Get-SMPLDomainRecords,  Add-SMPLDomainRecord, Update-SMPLDomainRecord, Remove-SMPLDomainRecord
---
+++ Sample-PowerDNSimpleScript.ps1
@@ -0,0 +1,11 @@
+﻿gcm -Module PowerDNSimple
+
+Get-SMPLDomains -emailAddress $DNSimpleEmailAddress -domainApiToken $DNSimpleToken
+
+Get-SMPLDomainRecords -domain <domain.com> -emailAddress $DNSimpleEmailAddress -domainApiToken $DNSimpleToken
+
+Add-SMPLDomainRecord -name "test" -recordType "A" -content "127.0.0.1" -domain <domain.com> -emailAddress $DNSimpleEmailAddress -domainApiToken $DNSimpleToken
+
+Update-SMPLDomainRecord -name test -content "127.0.0.1" -ttl 300 -domain <domain.com> -recordID <recordID> -emailAddress $DNSimpleEmailAddress -domainApiToken $DNSimpleToken
+
+Remove-SMPLDomainRecord -domain <domain.com> -recordID <recordID> -emailAddress $DNSimpleEmailAddress -domainApiToken $DNSimpleToken
